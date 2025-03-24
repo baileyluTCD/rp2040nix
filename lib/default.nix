15959cc28ff64cbf4421b20db8aa92ac0376dbb2
Luke Bailey <baileylu@tcd.ie>
2025-03-24 16:18:04 +0000

    Shortened lib package calling

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,12 @@
 {pkgs, ...}:
-with pkgs; {
+with pkgs; let
+  inherit (flake) pico-sdk pico-extras pico-host-sdl;
+in {
   mkRp2040App = callPackage ./mkRp2040App.nix {};
-  mkNativeApp = callPackage ./mkNativeApp.nix {};
+  mkNativeApp = callPackage ./mkNativeApp.nix {inherit pico-host-sdl;};
 
-  mkPicoApp = callPackage ./mkPicoApp.nix {inherit mkNativeApp mkRp2040App;};
+  mkPicoApp = callPackage ./mkPicoApp.nix {
+    inherit mkNativeApp mkRp2040App pico-sdk pico-extras;
+  };
 }
 
