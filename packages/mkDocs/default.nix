eaa4834bf9e9f26f2cbcd19f813e36892331cdbe
Luke Bailey <baileylu@tcd.ie>
2025-04-07 16:47:10 +0100

    Create basic implementation of documentation builder function

---
+++ packages/mkDocs/default.nix
@@ -0,0 +1,41 @@
+{pkgs, ...}: {
+  pname,
+  version,
+  src,
+  doxyfile ? ./Doxyfile,
+  ...
+} @ args: let
+  # Fetch nicer css styles
+  doxygen-awesome-css = pkgs.fetchFromGitHub {
+    owner = "jothepro";
+    repo = "doxygen-awesome-css";
+    tag = "v2.3.4";
+    hash = "sha256-HrFNbnpG0ttOg0xbC+WLhC77KmvkNjtVNk1QUD+JgYY=";
+  };
+in
+  pkgs.stdenv.mkDerivation ({
+      inherit pname src version;
+
+      nativeBuildInputs = with pkgs; [
+        doxygen
+        makeWrapper
+      ];
+
+      env.DOXYGEN_AWESOME_CSS = "${doxygen-awesome-css}/doxygen-awesome.css";
+
+      # Produce doxygen output files (html, latex, etc)
+      buildPhase = ''
+        doxygen ${doxyfile}
+      '';
+
+      # Put raw outputs in lib and create a http server binary in bin
+      installPhase = ''
+        mkdir -p $out/{bin,lib}
+
+        cp -R ./doxygen $out/lib
+
+        makeWrapper ${pkgs.httplz}/bin/httplz $out/bin/serve \
+          --chdir "$out/lib/html"
+      '';
+    }
+    // args)
